bc753b4b86d653d8d63ebc79603e6b1dac3cbb8e
slpatrycja <sl.patrycja.of@gmail.com>
2019-01-17 08:50:13 +0100

    refactor

--- public/my_app.rb
+++ public/my_app.rb
@@ -1,4 +1,36 @@
 class MyApp < Sinatra::Base
+  class MatrixUploadService
+    attr_reader :frame, :led_matrix, :options
+    def initialize(options)
+      @frame = LedMatrix::Frame.new
+      @led_matrix = LedMatrix::Client.new('192.168.1.56', 3000)
+      @options = options
+    end
+
+    def call
+      draw
+      upload
+    end
+
+    private
+
+    def upload
+      led_matrix.send_frame(frame)
+    end
+
+    def draw
+      picture_data.each.with_index do |row, y|
+        row.each.with_index do |col, x|
+          frame.set_pixel(x, y, col[0], col[1], col[2])
+        end
+      end
+    end
+
+    def picture_data
+      @picture_data ||= JSON.parse(options)
+    end
+  end
+
   get '/' do
     erb :'index.html'
   end
@@ -8,15 +40,6 @@ class MyApp < Sinatra::Base
   end
 
   post '/new' do
-    frame = LedMatrix::Frame.new
-    led_matrix = LedMatrix::Client.new('192.168.1.56', 3000)
-    data = JSON.parse(params[:data])
-
-    data.each.with_index do |row, y|
-      row.each.with_index do |col, x|
-        frame.set_pixel(x, y, col[0], col[1], col[2])
-      end
-    end
-    led_matrix.send_frame(frame)
+    MatrixUploadService.new(params[:data]).call
   end
 end
